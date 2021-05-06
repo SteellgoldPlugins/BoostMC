5e2bc44028863b5fd0b4ae1116644c1eed7dd46f
Gaëtan H <gaetanhus@gmail.com>
2021-05-06 08:41:18 +0200

    Add constant for money (TODO)

--- src/Steellg0ld/Museum/base/MPlayer.php
+++ src/Steellg0ld/Museum/base/MPlayer.php
@@ -8,6 +8,7 @@ class MPlayer extends Player {
 
     public string $faction_id = "";
     public string $faction_role = "";
+    public int $money = 0;
 
     public function getFaction(): MFaction {
         return new MFaction($this->faction_id);
@@ -18,5 +19,9 @@ class MPlayer extends Player {
      */
     public function hasFaction(): bool {
         return $this->faction_id !== null;
+    }
+
+    public function getMoney(): int{
+        return $this->money;
     }
 }
