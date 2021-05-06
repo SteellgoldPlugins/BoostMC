b772bc377068c4006335b5e4d2bce23dd6a951d2
Gaëtan H <steellgold0@gmail.com>
2021-05-06 19:39:53 +0200

    Register command and events

--- src/Steellg0ld/Museum/Plugin.php
+++ src/Steellg0ld/Museum/Plugin.php
@@ -5,12 +5,21 @@ namespace Steellg0ld\Museum;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use Steellg0ld\Museum\base\Database;
+use Steellg0ld\Museum\commands\faction\FactionCommand;
+use Steellg0ld\Museum\listeners\PlayerListeners;
+use Steellg0ld\Museum\utils\Utils;
 
 class Plugin extends PluginBase {
     public static $instance;
 
     public function onEnable(){
         self::$instance = $this;
+
+        $this->getServer()->getCommandMap()->registerAll("museum", [
+            new FactionCommand("f","§aOuvrir la page principale du plugin faction",["faction"])
+        ]);
+
+        $this->getServer()->getPluginManager()->registerEvents(new PlayerListeners(), $this);
     }
 
     /**
