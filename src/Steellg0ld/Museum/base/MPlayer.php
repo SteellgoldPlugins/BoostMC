4659db349da62d640598551f696f3ff20e179382
Gaëtan H <gaetanhus@gmail.com>
2021-05-06 12:42:14 +0200

    getRank and money

--- src/Steellg0ld/Museum/base/MPlayer.php
+++ src/Steellg0ld/Museum/base/MPlayer.php
@@ -6,9 +6,11 @@ use pocketmine\Player;
 
 class MPlayer extends Player {
 
+    public string $rank = "";
+    public int $money = 0;
+
     public string $faction_id = "";
     public string $faction_role = "";
-    public int $money = 0;
 
     public function getFaction(): MFaction {
         return new MFaction($this->faction_id);
@@ -21,7 +23,21 @@ class MPlayer extends Player {
         return $this->faction_id !== null;
     }
 
+    /**
+     * @return int
+     */
     public function getMoney(): int{
         return $this->money;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRank(): string{
+        return $this->rank;
+    }
+
+    public function hasRank(String ...$ranks): bool{
+        return in_array($this->rank, $ranks);
     }
 }
