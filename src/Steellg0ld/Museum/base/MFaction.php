0b2d7bde432b7568424218fe1ac1044ac994c96c
Gaëtan H <steellgold0@gmail.com>
2021-05-06 20:42:12 +0200

    MFaction (TODO)

--- src/Steellg0ld/Museum/base/MFaction.php
+++ src/Steellg0ld/Museum/base/MFaction.php
@@ -5,29 +5,14 @@ namespace Steellg0ld\Museum\base;
 use Steellg0ld\Museum\Plugin;
 
 class MFaction{
-
-    public string $faction_id;
-    public string $faction_name;
-
-    public function __construct(String $factionId){
-        $this->faction_id = $factionId;
-        $this->faction_name = "LesProduitsLaitiers";
-    }
+    public $data;
 
     /**
-     * @param String $faction
-     * @return bool
+     * MFaction constructor.
+     * @param String $faction_id
      */
-    public static function factionExist(String $faction): bool{
-        $factions = Plugin::getInstance()->getFactions();
-        return $factions->exists($faction);
-    }
-
-    /**
-     * @return bool
-     */
-    public function getName(): bool{
-        return (self::factionExist($this->faction_name) ? $this->faction_name : "Faction introuvable");
+    public function __construct(String $faction_id){
+        $this->data = Plugin::getInstance()->getDatabase()->getFactionData($faction_id);
     }
 
     /**
@@ -45,6 +30,13 @@ class MFaction{
      */
     public function getFactionAccess(MPlayer $player, string ...$role): string {
         return in_array($player->faction_role, $role) ? "§a§lACCÈS" : "§c§lNON-ACCÈS";
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName(){
+        return $this->data["name"];
     }
 
     /**
