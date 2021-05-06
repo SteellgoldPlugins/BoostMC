515ae7425a4fe50c0c61c356eb450d2df84bcb6f
Gaëtan H <steellgold0@gmail.com>
2021-05-06 20:42:25 +0200

    Faction form

--- src/Steellg0ld/Museum/forms/FactionForm.php
+++ src/Steellg0ld/Museum/forms/FactionForm.php
@@ -20,14 +20,17 @@ class FactionForm {
                         if($p->getMoney() >= Utils::FACTION_CREATE_PRICE){
                             if(isset($data[1])){
                                 if(!MFaction::factionExist($data[1])){
-                                    $faction_id = uniqid("FACTION-");
+                                    $faction_id = uniqid();
                                     $p->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}Vous venez de créer la faction nommé {PRIMARY}".$data[1]));
 
                                     $factions = Plugin::getInstance()->getFactions();
-                                    $factions->set($faction_id, $p->getName());
+                                    $factions->set($data[1],$faction_id);
                                     $factions->save();
 
-                                    if($data[2]) Server::getInstance()->broadcastMessage(Utils::createMessage("{DANGER}- {SECONDARY}Une faction nommé {DANGER}" . $data[1] . " {SECONDARY}, viens d'être créer par {DANGER}" . $p->getName()));
+                                    $p->money = $p->money - Utils::FACTION_CREATE_PRICE;
+                                    $p->faction_id = $faction_id;
+                                    Plugin::getInstance()->getDatabase()->factionRegister($faction_id,$data[1],$p->getName());
+                                    if($data[2]) Server::getInstance()->broadcastMessage(Utils::createMessage("{DANGER}- {SECONDARY}Une faction nommé {DANGER}" . $data[1] . "{SECONDARY}, viens d'être créer par {DANGER}" . $p->getName()));
                                 }else{
                                     $p->sendMessage(Utils::createMessage("{ERROR}- {SECONDARY}La faction {ERROR}" . $data[1] . " {SECONDARY}existe déjà !"));
                                 }
@@ -63,7 +66,7 @@ class FactionForm {
             $form->setContent(Utils::createMessage("{PRIMARY}- {SECONDARY}Votre faction: {PRIMARY}".$player->getFaction()->getName()."{SECONDARY}")."\n".
                 Utils::createMessage("{PRIMARY}> {SECONDARY}Claim(s) actif(s): {SECONDARY}".$player->getFaction()->getFactionClaims(true))."\n".
                 Utils::createMessage("{PRIMARY}> {SECONDARY}Claim(s) inactif(s): {SECONDARY}".$player->getFaction()->getFactionClaims(false)));
-            $form->addButton("§cQuitter la {FACTION}" . $player->getFaction()->getName());
+            $form->addButton("§cQuitter la faction\n§c§l" . $player->getFaction()->getName());
             $form->addButton("Gérer les membres\n" . $player->getFaction()->getFactionAccess($player));
             $form->addButton("Gérer la faction\n" . $player->getFaction()->getFactionAccess($player));
             $form->addButton("Gérer les claims\n" . $player->getFaction()->getFactionAccess($player));
