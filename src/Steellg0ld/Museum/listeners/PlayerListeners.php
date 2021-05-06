57e6467493564a19c11887f6772c7ce67d5da87f
Gaëtan H <steellgold0@gmail.com>
2021-05-06 20:42:33 +0200

    Messages events

--- src/Steellg0ld/Museum/listeners/PlayerListeners.php
+++ src/Steellg0ld/Museum/listeners/PlayerListeners.php
@@ -7,6 +7,7 @@ use pocketmine\event\player\PlayerCreationEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use Steellg0ld\Museum\base\MPlayer;
+use Steellg0ld\Museum\Plugin;
 
 class PlayerListeners implements Listener {
     public function onCreation(PlayerCreationEvent $event){
@@ -15,12 +16,16 @@ class PlayerListeners implements Listener {
 
     public function onJoin(PlayerJoinEvent $event){
         $player = $event->getPlayer();
+        $event->setJoinMessage("");
         if(!$player instanceof MPlayer) return;
         if(!$player->hasPlayedBefore()) $player->register();
         $player->dataConnect();
     }
 
     public function onQuit(PlayerQuitEvent $event){
-
+        $event->setQuitMessage("");
+        $player = $event->getPlayer();
+        if(!$player instanceof MPlayer) return;
+        Plugin::getInstance()->getDatabase()->updatePlayer($player->getName(),$player->rank, $player->money, $player->faction_id);
     }
 }
