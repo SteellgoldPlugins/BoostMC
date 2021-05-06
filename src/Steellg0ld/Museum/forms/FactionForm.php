b34d47416516aad5af2a963d77471d8d316e79d8
Gaëtan H <gaetanhus@gmail.com>
2021-05-06 08:41:28 +0200

    Faction creation form code

--- src/Steellg0ld/Museum/forms/FactionForm.php
+++ src/Steellg0ld/Museum/forms/FactionForm.php
@@ -2,6 +2,7 @@
 
 namespace Steellg0ld\Museum\forms;
 
+use pocketmine\Server;
 use Steellg0ld\Museum\base\MPlayer;
 use Steellg0ld\Museum\forms\api\CustomForm;
 use Steellg0ld\Museum\forms\api\Form;
@@ -14,13 +15,23 @@ class FactionForm {
             $form = new CustomForm(
                 function (MPlayer $p, $data) {
                     if ($data !== null) {
-
+                        if($p->getMoney() >= Utils::FACTION_CREATE_PRICE){
+                            if(isset($data[1])){
+                                $p->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}Vous venez de créer la faction nommé {PRIMARY}".$data[1]));
+                                if($data[2]) Server::getInstance()->broadcastMessage(Utils::createMessage("{DANGER}- {SECONDARY}Une faction nommé {DANGER}" . $data[1] . " {SECONDARY}, viens d'être créer par {DANGER}" . $p->getName()));
+                            }else{
+                                $p->sendMessage("{ERROR}- {SECONDARY}Vous n'avez pas préciser le nom de {ERROR}votre faction");
+                            }
+                        }else{
+                            $p->sendMessage("{ERROR}- {SECONDARY}Vous n'avez pas assez d'argent pour pouvoir {ERROR}créer une faction");
+                        }
                     }
                 }
             );
 
             $form->setTitle(Form::FACTION_TITLE);
-            $form->addInput("Nom de la faction");
+            $form->addLabel(Utils::createMessage("{PRIMARY}- {SECONDARY}Pour créer une faction cela vous coûtera {PRIMARY}500{ECONOMY_SYMBOL}"));
+            $form->addInput("Nom de la faction","LesProduitsLaitiers");
             $player->sendForm($form);
         }
     }
