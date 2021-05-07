947ec41b54027cd6d87761cd4d8a4d3084769a0e
Gaëtan H <steellgold0@gmail.com>
2021-05-07 21:03:45 +0200

    "Sponsor" Player Code

---
+++ src/Steellg0ld/Museum/commands/CodeCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Steellg0ld\Museum\commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use Steellg0ld\Museum\base\MPlayer;
+use Steellg0ld\Museum\forms\CodeForm;
+use Steellg0ld\Museum\utils\Utils;
+
+class CodeCommand extends Command{
+    public function __construct(string $name, string $description, array $aliases){
+        parent::__construct($name, $description, "", $aliases);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if(!$sender instanceof MPlayer){
+            $sender->sendMessage(Utils::createMessage(Utils::CONSOLE_ERROR));
+            return;
+        }
+
+        CodeForm::open($sender);
+    }
+}
