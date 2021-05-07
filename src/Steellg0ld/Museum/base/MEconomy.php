8778bed4861bca53b8759e6bac9b3ca678132287
Gaëtan H <steellgold0@gmail.com>
2021-05-07 07:31:39 +0200

    Add money

--- src/Steellg0ld/Museum/base/MEconomy.php
+++ src/Steellg0ld/Museum/base/MEconomy.php
@@ -5,8 +5,13 @@ namespace  Steellg0ld\Museum\base;
 use Steellg0ld\Museum\utils\Utils;
 
 class MEconomy {
-    public function delMoney(MPlayer  $player, Int $count){
+    public function delMoney(MPlayer  $player, Int $count, Bool $msg = false){
         $player->money = $player->money - $count;
-        $player->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}La somme de {PRIMARY}".$count.Utils::ECONOMY_SYMBOL."{SECONDARY}, vous a été retiré(e) vous avez désormais {PRIMARY}".$player->money.Utils::ECONOMY_SYMBOL));
+        if($msg == true) $player->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}La somme de {PRIMARY}".$count.Utils::ECONOMY_SYMBOL."{SECONDARY}, vous a été retiré(e) vous avez désormais {PRIMARY}".$player->money.Utils::ECONOMY_SYMBOL));
+    }
+
+    public function addMoney(MPlayer  $player, Int $count, Bool $msg = false){
+        $player->money = $player->money + $count;
+        if($msg == true) $player->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}La somme de {PRIMARY}".$count.Utils::ECONOMY_SYMBOL."{SECONDARY}, vous a été ajouté, vous avez désormais {PRIMARY}".$player->money.Utils::ECONOMY_SYMBOL));
     }
 }
