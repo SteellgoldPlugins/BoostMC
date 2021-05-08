65b7d887138acf98d92cfa234e2713dd7eacb95e
Gaëtan H <steellgold0@gmail.com>
2021-05-08 13:27:35 +0200

    Why this file please...

---
+++ Museum OF/src/Steellg0ld/Museum/commands/faction/subcommands/KickSubCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Steellg0ld\Museum\commands\faction\subcommands;
+
+use Steellg0ld\Museum\base\MFaction;
+use Steellg0ld\Museum\base\MPlayer;
+
+class KickSubCommand
+{
+    public function execute(MPlayer $toKick, MFaction $faction, Array $kickInformations) {
+        $faction->removeMember($toKick);
+    }
+}
