8edb8519e05a32499f1fcd06b3fa83fc52ba1fed
Gaëtan H <steellgold0@gmail.com>
2021-05-08 13:27:43 +0200

    Invite Sub Command Reformat

--- Museum OF/src/Steellg0ld/Museum/commands/faction/subcommands/InviteSubCommand.php
+++ Museum OF/src/Steellg0ld/Museum/commands/faction/subcommands/InviteSubCommand.php
@@ -1,9 +1,7 @@
 <?php
- // KESTU FOU
+
 namespace Steellg0ld\Museum\commands\faction\subcommands;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\Server;
 use Steellg0ld\Museum\base\MFaction;
 use Steellg0ld\Museum\base\MPlayer;
@@ -19,17 +17,12 @@ class InviteSubCommand{
         $invitor = Server::getInstance()->getPlayer($invited->invitations_infos["invitor"]);
         if($invitor instanceof MPlayer) $faction = $invitor->getFaction();
 
-        var_dump("1");
         if($invited->invitations_infos["expiration"] >= time()){
-            var_dump("2");
+            $invited->faction_id = $invitor->getFaction()->getIdentifier();
             $invited->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}Vous avez accepté(e) la demande, bienvenue dans la faction {PRIMARY}{FACTION}{SECONDARY}, en tant que {PRIMARY}{ROLE}", ["{FACTION}", "{ROLE}"], [$invited->invitations_infos["faction"], MFaction::ROLES[$invited->invitations_infos["role"]]]));
-            if($invitor instanceof MPlayer) $invitor->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}Le joueur {PRIMARY}{NAME} a accepté votre invitation, il est désormais dans votre faction en tant que {PRIMARY}{ROLE}", ["{ROLE}", "{FACTION}"], [MFaction::ROLES[$invited->invitations_infos["role"]]]));
+            if($invitor instanceof MPlayer) $invitor->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}Le joueur {PRIMARY}{NAME}{SECONDARY} a accepté votre invitation, il est désormais dans votre faction en tant que {PRIMARY}{ROLE}", ["{NAME}", "{ROLE}", "{FACTION}"], [$invited->getName(), MFaction::ROLES[$invited->invitations_infos["role"]]]));
 
-            var_dump("3");
-            $members = explode(" ", $faction->data["members"]);
-            array_push($members, $invited->getName());
-            $members = implode(" ",$members);
-            Plugin::getInstance()->getDatabase()->updateFactionMembers($members, $faction->data["identifier"]);
+            $faction->addMember($invited->getName());
         }else{
             $invited->sendMessage(Utils::createMessage("{ERROR}- {SECONDARY}L'invitation de {NAME} à expirée, veuillez lui demander de vous réinviter", ["{NAME}"], [$invited->invitations_infos["invitor"]]));
             if($invitor instanceof MPlayer) $invitor->sendMessage(Utils::createMessage("{ERROR}- {SECONDARY}L'invitation à expirée"));
