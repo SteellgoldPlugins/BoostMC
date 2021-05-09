a77e40af83d1592a5228354a7f439cb27251b414
Gaëtan H <steellgold0@gmail.com>
2021-05-09 13:07:23 +0200

    Registry Commands

---
+++ Bot Discord/index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+
+/**
+exports.MySQL = require("mysql");
+exports.Connection = exports.MySQL.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'stonkser'
+});
+
+exports.Connection.connect(function(err) {
+    if (err) { return console.error('Error in the connection: ' + err.stack); }
+    console.log(exports.Colors.rainbow('Connecté à la base de données avec l\'identifiant: ' + exports.Connection.threadId));
+});
+**/
+
+exports.Version = "v1.0"
+
+exports.Client = new Discord.Client()
+exports.Client.commands = new Discord.Collection();
+exports.Config = require('./resources/Config.json')
+exports.Colors = require('colors')
+
+const CommandRegistry = require('./commands/CommandRegister')
+CommandRegistry.registerCommand(exports.Client.commands, "link", true, "commands/minecraft/", "LinkCommand", exports.Colors)
+
+require('./listener/members/Messages')
+
+console.log(exports.Colors.rainbow("Museum Bot Oppérationel"))
+exports.Client.login(exports.Config.token)
