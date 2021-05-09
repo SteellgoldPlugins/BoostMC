2ef129786be9748cbfd1725bae2794b880520aa1
Gaëtan H <steellgold0@gmail.com>
2021-05-09 02:45:18 +0200

    Fuck this white night

--- Museum OF/src/Steellg0ld/Museum/Plugin.php
+++ Museum OF/src/Steellg0ld/Museum/Plugin.php
@@ -3,58 +3,86 @@
 namespace Steellg0ld\Museum;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Tile;
 use pocketmine\utils\Config;
+use Steellg0ld\Museum\api\Claims;
 use Steellg0ld\Museum\base\Database;
 use Steellg0ld\Museum\base\MEconomy;
+use Steellg0ld\Museum\base\MFaction;
 use Steellg0ld\Museum\commands\CodeCommand;
 use Steellg0ld\Museum\commands\faction\FactionCommand;
-use Steellg0ld\Museum\commands\faction\InviteSubCommand;
 use Steellg0ld\Museum\listeners\PlayerListeners;
 
-class Plugin extends PluginBase {
+class Plugin extends PluginBase
+{
     public static $instance;
+    // public static array $claims;
 
-    public function onEnable(){
+    public function onEnable()
+    {
         self::$instance = $this;
         $this->getDatabase()->init();
 
         $this->getServer()->getCommandMap()->registerAll("museum", [
-            new FactionCommand("f","§aOuvrir la page principale du plugin faction",["faction"]),
-            new CodeCommand("c","§aOuvrir la page pour utiliser un code",["code"])
+            new FactionCommand("f", "§aOuvrir la page principale du plugin faction", ["faction"]),
+            new CodeCommand("c", "§aOuvrir la page pour utiliser un code", ["code"])
         ]);
 
+        $this->getClaims()->initClaim();
         $this->getServer()->getPluginManager()->registerEvents(new PlayerListeners(), $this);
+    }
+
+    public function onDisable()
+    {
+        /**
+        foreach ($this->getClaims()->getClaims() as $claim) {
+        var_dump($claim["ccc"]);
+        }
+         */
+    }
+
+    /**
+     * @return Database
+     */
+    public function getDatabase(): Database
+    {
+        return new Database();
+    }
+
+    /**
+     * @return Config
+     */
+    public function getFactions(): Config
+    {
+        return new Config(Plugin::getInstance()->getDataFolder() . "factions.yml", Config::YAML);
     }
 
     /**
      * @return mixed
      */
-    public static function getInstance() : Plugin {
+    public static function getInstance(): Plugin
+    {
         return self::$instance;
     }
 
     /**
      * @return Config
      */
-    public function getFactions() : Config {
-        return new Config(Plugin::getInstance()->getDataFolder() . "factions.yml", Config::YAML);
-    }
-
-    /**
-     * @return Config
-     */
-    public function getCodes() : Config {
+    public function getCodes(): Config
+    {
         return new Config(Plugin::getInstance()->getDataFolder() . "codes.yml", Config::YAML);
     }
 
-    public function getEconomyAPI(): MEconomy{
-        return new MEconomy();
+    /**
+     * @return Claims
+     */
+    public function getClaims(): Claims
+    {
+        return new Claims();
     }
 
-    /**
-     * @return Database
-     */
-    public function getDatabase(): Database{
-        return new Database();
+    public function getEconomyAPI(): MEconomy
+    {
+        return new MEconomy();
     }
 }
