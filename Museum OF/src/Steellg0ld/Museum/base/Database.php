5b0bb8c9323ecc856fe69ed2d0375a42eb00cb0e
Gaëtan H <steellgold0@gmail.com>
2021-05-09 02:40:25 +0200

    Database update for Claims

--- Museum OF/src/Steellg0ld/Museum/base/Database.php
+++ Museum OF/src/Steellg0ld/Museum/base/Database.php
@@ -3,28 +3,30 @@
 namespace Steellg0ld\Museum\base;
 
 
-use pocketmine\nbt\tag\StringTag;
-use pocketmine\Server;
 use Steellg0ld\Museum\Plugin;
 
-class Database{
+class Database
+{
+    public function init()
+    {
+        $this->getDatabase()->query("CREATE TABLE IF NOT EXISTS players (player TEXT, address TEXT, rank INT, money INT, faction TEXT, faction_role INT, code TEXT, hasJoinedWithCode BOOL, enterCodeWaitEnd TEXT)");
+        $this->getDatabase()->query("CREATE TABLE IF NOT EXISTS factions (identifier TEXT, name TEXT, members TEXT, owner TEXT, claims TEXT)");
+    }
+
     /**
      * @return \SQLite3
      */
-    public function getDatabase(): \SQLite3 {
+    public function getDatabase(): \SQLite3
+    {
         return new \SQLite3(Plugin::getInstance()->getDataFolder() . "Database.db");
-    }
-
-    public function init(){
-        $this->getDatabase()->query("CREATE TABLE IF NOT EXISTS players (player TEXT, address TEXT, rank INT, money INT, faction TEXT, faction_role INT, code TEXT, hasJoinedWithCode BOOL, enterCodeWaitEnd TEXT)");
-        $this->getDatabase()->query("CREATE TABLE IF NOT EXISTS factions (identifier TEXT, name TEXT, members TEXT, owner TEXT)");
     }
 
     /**
      * @param String $name
      * @param String $address
      */
-    public function playerRegister(String $name, String $address){
+    public function playerRegister(string $name, string $address)
+    {
         $time = time() + 60 * 60 * 24 * 3;
         $address = base64_encode(base64_encode(base64_encode(base64_encode($address))));
         $this->getDatabase()->query("INSERT INTO players (player, address, rank, money, faction, faction_role, code, hasJoinedWithCode, enterCodeWaitEnd) VALUES ('$name', '$address', 0, 0, 'none', 0, 'none', 'none', '$time')");
@@ -35,23 +37,26 @@ class Database{
      * @param String $name
      * @param String $owner
      */
-    public function factionRegister(String $identifier, String $name, String $owner){
+    public function factionRegister(string $identifier, string $name, string $owner)
+    {
         $factions = Plugin::getInstance()->getFactions();
-        $factions->set($name,$identifier);
+        $factions->set($name, $identifier);
         $factions->save();
 
-        $this->getDatabase()->query("INSERT INTO factions (identifier, name, members, owner) VALUES ('$identifier', '$name', '$owner', '$owner')");
+        $claims = base64_encode(serialize(array()));
+        $this->getDatabase()->query("INSERT INTO factions (identifier, name, members, owner, claims) VALUES ('$identifier', '$name', '$owner', '$owner', '$claims')");
     }
 
     /**
      * @param String $name
      * @return mixed
      */
-    public function getPlayerData(String $name){
+    public function getPlayerData(string $name)
+    {
         $data = array();
         $query = self::getDatabase()->query("SELECT * FROM players WHERE player = '$name'");
-        while ($res = $query->fetchArray(1)){
-            array_push($data,$res);
+        while ($res = $query->fetchArray(1)) {
+            array_push($data, $res);
         }
         return $data[0];
     }
@@ -60,11 +65,12 @@ class Database{
      * @param string $faction_id
      * @return mixed
      */
-    public function getFactionData(string $faction_id){
+    public function getFactionData(string $faction_id)
+    {
         $data = array();
         $query = self::getDatabase()->query("SELECT * FROM factions WHERE identifier = '$faction_id'");
-        while ($res = $query->fetchArray(1)){
-            array_push($data,$res);
+        while ($res = $query->fetchArray(1)) {
+            array_push($data, $res);
         }
         return $data[0];
     }
@@ -78,7 +84,8 @@ class Database{
      * @param bool $hasJoinedWithCode
      * @param string $enterCodeWaitEnd
      */
-    public function updatePlayer(String $name, Int $rank = 0, Int $money = 250, String $faction = "none", Int $faction_role = 0, String $code = "none", Bool $hasJoinedWithCode = false, String $enterCodeWaitEnd = "0"){
+    public function updatePlayer(string $name, int $rank = 0, int $money = 250, string $faction = "none", int $faction_role = 0, string $code = "none", bool $hasJoinedWithCode = false, string $enterCodeWaitEnd = "0")
+    {
         $this->getDatabase()->query("UPDATE players SET rank = '$rank', money = '$money', faction = '$faction', faction_role = '$faction_role', code = '$code$', hasJoinedWithCode = '$hasJoinedWithCode', enterCodeWaitEnd = '$enterCodeWaitEnd' WHERE player = '$name'");
     }
 
@@ -86,7 +93,8 @@ class Database{
      * @param String $members
      * @param String $faction
      */
-    public function updateFactionMembers(String $members, String $faction){
+    public function updateFactionMembers(string $members, string $faction)
+    {
         $this->getDatabase()->query("UPDATE factions SET members = '$members' WHERE identifier = '$faction'");
     }
 }
