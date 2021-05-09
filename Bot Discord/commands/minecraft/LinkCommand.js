cd4c2f8ba29b61ff9d86036ebee918dd9196b089
Gaëtan H <steellgold0@gmail.com>
2021-05-09 14:27:52 +0200

    Link File Command

---
+++ Bot Discord/commands/minecraft/LinkCommand.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js");
+const { Client, Colors, Version, Embed, Commands} = require("../../index");
+
+module.exports.run = async (Client, message, args) => {
+    if(!message.guild) return;
+}
