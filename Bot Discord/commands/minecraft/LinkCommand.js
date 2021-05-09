4b8c5c10721a1a64ce98793fb2921e45a0faf9c9
UnNyanCat <67955616+UnNyanCat@users.noreply.github.com>
2021-05-09 16:29:32 +0200

    azazazazazazazazaza

---
+++ Bot Discord/commands/minecraft/LinkCommand.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js')
+const { } = require('../../index');
+
+module.exports.run = async (Client, message, args) => {
+    if(!message.guild) return;
+    console.log("A faire");
+}
