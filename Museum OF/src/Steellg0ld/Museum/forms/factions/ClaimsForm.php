5430b6d265b90f3f89d9405e56b4368caa4dceb9
Gaëtan H <steellgold0@gmail.com>
2021-05-09 14:28:55 +0200

    Claim Form Message when enter the claim (TODO)

--- Museum OF/src/Steellg0ld/Museum/forms/factions/ClaimsForm.php
+++ Museum OF/src/Steellg0ld/Museum/forms/factions/ClaimsForm.php
@@ -2,6 +2,7 @@
 
 namespace Steellg0ld\Museum\forms\factions;
 
+use Steellg0ld\Museum\base\MFaction;
 use Steellg0ld\Museum\base\MPlayer;
 use Steellg0ld\Museum\forms\api\CustomForm;
 use Steellg0ld\Museum\forms\api\Form;
@@ -17,7 +18,22 @@ class ClaimsForm
                 function (MPlayer $p, $data) {
                     if ($data !== null) {
                         if ($p->faction_role >= 2) {
-                            Plugin::getInstance()->getClaims()->claim($p,$p->getFaction()->getIdentifier());
+                            $toPos = $p->getLevel()->getChunkAtPosition($p->asVector3());
+                            $chunkXP = $toPos->getX();
+                            $chunkZP = $toPos->getZ();
+
+                            if($data[1] !== $p->getFaction()->getName()) {
+                                $msg = base64_encode($data[1]);
+                            }else{
+                                $msg = null;
+                            }
+
+                            if(!Plugin::getInstance()->getClaims()->isInClaim($p->getLevel(), $chunkXP, $chunkZP)){
+                                Plugin::getInstance()->getClaims()->claim($p,$p->getFaction()->getIdentifier(),$msg);
+                                $p->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}Vous venez de protéger le chunk {PRIMARY}$chunkXP{SECONDARY}:{PRIMARY}$chunkZP"));
+                            }else{
+                                $p->sendMessage(Utils::createMessage("{ERROR}- {SECONDARY}La zone est déjà protéger par la faction {ERROR}".MFaction::getNameByIdentifier(Plugin::getInstance()->getClaims()->getFactionClaim($p->getLevel(), $chunkXP, $chunkZP))));
+                            }
                             var_dump(Plugin::getInstance()->getClaims()->getClaims());
                         } else {
                             $p->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}Vous n'avez pas la rôle requis pour pouvoir protéger des zones"));
