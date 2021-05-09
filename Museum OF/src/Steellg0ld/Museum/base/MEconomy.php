01e47c99e4a22ec58072acb6fa2182f4a4d0261e
Gaëtan H <steellgold0@gmail.com>
2021-05-09 02:40:40 +0200

    Update Economy namespace and messages

--- Museum OF/src/Steellg0ld/Museum/base/MEconomy.php
+++ Museum OF/src/Steellg0ld/Museum/base/MEconomy.php
@@ -1,17 +1,20 @@
 <?php
 
-namespace  Steellg0ld\Museum\base;
+namespace Steellg0ld\Museum\base;
 
 use Steellg0ld\Museum\utils\Utils;
 
-class MEconomy {
-    public function delMoney(MPlayer $player, Int $count, Bool $msg = false){
+class MEconomy
+{
+    public function delMoney(MPlayer $player, int $count, bool $msg = false)
+    {
         $player->money = $player->money - $count;
-        if($msg == true) $player->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}La somme de {PRIMARY}".$count.Utils::ECONOMY_SYMBOL."{SECONDARY}, vous a été retiré(e) vous avez désormais {PRIMARY}".$player->money.Utils::ECONOMY_SYMBOL));
+        if ($msg == true) $player->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}La somme de {PRIMARY}" . $count . Utils::ECONOMY_SYMBOL . "{SECONDARY}, vous a été retiré(e) vous avez désormais {PRIMARY}" . $player->money . Utils::ECONOMY_SYMBOL));
     }
 
-    public function addMoney(MPlayer $player, Int $count, Bool $msg = false){
+    public function addMoney(MPlayer $player, int $count, bool $msg = false)
+    {
         $player->money = $player->money + $count;
-        if($msg == true) $player->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}La somme de {PRIMARY}".$count.Utils::ECONOMY_SYMBOL."{SECONDARY}, vous a été ajouté, vous avez désormais {PRIMARY}".$player->money.Utils::ECONOMY_SYMBOL));
+        if ($msg == true) $player->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}La somme de {PRIMARY}" . $count . Utils::ECONOMY_SYMBOL . "{SECONDARY}, vous a été ajouté, vous avez désormais {PRIMARY}" . $player->money . Utils::ECONOMY_SYMBOL));
     }
 }
