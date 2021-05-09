8c646d7ab0c963025596bd2edfbe9db412f46f2b
Gaëtan H <steellgold0@gmail.com>
2021-05-09 02:43:59 +0200

    ModalForm Button Update

--- Museum OF/src/Steellg0ld/Museum/forms/api/ModalForm.php
+++ Museum OF/src/Steellg0ld/Museum/forms/api/ModalForm.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace Steellg0ld\Museum\forms\api;
 
-class ModalForm extends Form {
+class ModalForm extends Form
+{
 
     /** @var string */
     private $content = "";
@@ -9,7 +11,8 @@ class ModalForm extends Form {
     /**
      * @param callable|null $callable
      */
-    public function __construct(?callable $callable) {
+    public function __construct(?callable $callable)
+    {
         parent::__construct($callable);
         $this->data["type"] = "modal";
         $this->data["title"] = "";
@@ -21,56 +24,64 @@ class ModalForm extends Form {
     /**
      * @param string $title
      */
-    public function setTitle(string $title) : void {
+    public function setTitle(string $title): void
+    {
         $this->data["title"] = $title;
     }
 
     /**
      * @return string
      */
-    public function getTitle() : string {
+    public function getTitle(): string
+    {
         return $this->data["title"];
     }
 
     /**
      * @return string
      */
-    public function getContent() : string {
+    public function getContent(): string
+    {
         return $this->data["content"];
     }
 
     /**
      * @param string $content
      */
-    public function setContent(string $content) : void {
+    public function setContent(string $content): void
+    {
         $this->data["content"] = $content;
     }
 
     /**
      * @param string $text
      */
-    public function setButton1(string $text) : void {
+    public function setButton1(string $text): void
+    {
         $this->data["button1"] = $text;
     }
 
     /**
      * @return string
      */
-    public function getButton1() : string {
+    public function getButton1(): string
+    {
         return $this->data["button1"];
     }
 
     /**
      * @param string $text
      */
-    public function setButton2(string $text) : void {
+    public function setButton2(string $text): void
+    {
         $this->data["button2"] = $text;
     }
 
     /**
      * @return string
      */
-    public function getButton2() : string {
+    public function getButton2(): string
+    {
         return $this->data["button2"];
     }
 }
