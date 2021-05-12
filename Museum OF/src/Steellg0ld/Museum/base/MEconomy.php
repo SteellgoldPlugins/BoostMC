61c5f0a6d0e05006c9406e76628409a816a2d7b3
Gaëtan H <steellgold0@gmail.com>
2021-05-12 11:09:34 +0200

    Shop Variable Economy

--- Museum OF/src/Steellg0ld/Museum/base/MEconomy.php
+++ Museum OF/src/Steellg0ld/Museum/base/MEconomy.php
@@ -6,6 +6,8 @@ use Steellg0ld\Museum\utils\Utils;
 
 class MEconomy
 {
+    public static $shop = [};
+                           
     public function delMoney(MPlayer $player, int $count, bool $msg = false)
     {
         $player->money = $player->money - $count;
@@ -16,5 +18,10 @@ class MEconomy
     {
         $player->money = $player->money + $count;
         if ($msg == true) $player->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}La somme de {PRIMARY}" . $count . Utils::ECONOMY_SYMBOL . "{SECONDARY}, vous a été ajouté, vous avez désormais {PRIMARY}" . $player->money . Utils::ECONOMY_SYMBOL));
+    }
+
+    public function getItemPrice(MPlayer $player, int $count, bool $msg = false)
+    {
+        // TODO
     }
 }
