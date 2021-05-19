e36da1a07a4158a8335955b3f9f7e6b65eba9994
Gaëtan H <steellgold0@gmail.com>
2021-05-19 16:32:06 +0200

    DataProvider Default Data Return

--- Museum OF/src/Steellg0ld/Museum/json/data/DataProvider.php
+++ Museum OF/src/Steellg0ld/Museum/json/data/DataProvider.php
@@ -5,18 +5,23 @@ namespace Steellg0ld\Museum\json\data;
 
 use Steellg0ld\Museum\base\MPlayer;
 use Steellg0ld\Museum\json\JSONProvider;
+use Steellg0ld\Museum\Plugin;
 
 class DataProvider
 {
     public function createFaction(string $uniqid, MPlayer $owner, string $name, string $description, array $members, int $power){
         $provider = new JSONProvider();
+        $factions = Plugin::getInstance()->getConfigFile("factions");
+        $factions->set($owner->getName(),$uniqid);
+        $factions->save();
+
         $data = $provider->getFactionConfig($uniqid);
         $data->set($uniqid, $this->defaultData($uniqid, $owner->getName(), $name, $description, $members, $power));
         $data->save();
     }
 
     public function defaultData(string $uniqid, String $owner_name = "Server", string $name = "Default Name", string $description = "Default Faction Description", array $members = [], int $power = 20) : array{
-        return $default_data = [
+        return [
             "name" => $name,
             "uniqid" => $uniqid,
             "description" => $description,
