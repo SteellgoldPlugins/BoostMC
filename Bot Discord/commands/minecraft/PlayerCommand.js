7086526865ca01d9f7f5af78a825b184a80126d8
Gaëtan H <steellgold0@gmail.com>
2021-05-22 23:07:24 +0200

    Player Info Command

--- Bot Discord/commands/minecraft/PlayerCommand.js
+++ Bot Discord/commands/minecraft/PlayerCommand.js
@@ -1,8 +1,20 @@
 const Discord = require("discord.js");
-const { Database } = require("../../index");
+const { Database, fs} = require("../../index");
 
 module.exports.run = async (Client, message, args) => {
     if(!message.guild) return;
+
+    const ROLES = {
+        0: "-",
+        1: "*",
+        2: "**",
+        3: "***",
+    }
+
+    const RANKS = {
+        0: "Joueur",
+        1: "Premium",
+    }
 
     if(!args[0]) return message.channel.send("Vous n'avez pas préciser de pseudo d'un joueur")
 
@@ -11,11 +23,16 @@ module.exports.run = async (Client, message, args) => {
             console.error(err.message);
             return message.channel.send(`Aucun joueur n'existe avec le pseudo **${args[0]}**`)
         }else{
+            if(row.faction !== "none") faction = JSON.parse(fs.readFileSync('./../' + row.faction + '.json','utf8'));
+
             const embed = new Discord.MessageEmbed()
             embed.setTitle("Informations")
             embed.setDescription(`Voici les informations du joueur **${args[0]}**`)
-            embed.addField("() Faction",row.faction, true)
-            embed.addField("() Argent",row.money  + "$", true)
+            embed.addField('Grade', RANKS[row.rank], true)
+            embed.addField('Temps de jeu', "1j30m02s", true)
+            embed.addField('Compte Discord', row.discordId === "none" ? "Aucun compte lié" : "<@" + row.discordId + ">", true)
+            embed.addField('Faction', row.faction === "none" ? "Aucune faction" : faction['60a935469a262']["name"] + ROLES[row.faction_role], true)
+            embed.addField('Argent', row.money + '$', true)
             message.channel.send(embed)
         }
     });
