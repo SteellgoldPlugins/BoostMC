cf64faf2041908d4cf893d68e511d3455e736dff
Gaëtan H <steellgold0@gmail.com>
2021-05-22 21:29:20 +0200

    Chest

--- Museum OF/src/Steellg0ld/Museum/listeners/FactionListeners.php
+++ Museum OF/src/Steellg0ld/Museum/listeners/FactionListeners.php
@@ -2,13 +2,17 @@
 
 namespace Steellg0ld\Museum\listeners;
 
+use pocketmine\block\Chest;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\Event;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
 use Steellg0ld\Museum\base\MFaction;
 use Steellg0ld\Museum\base\MPlayer;
+use Steellg0ld\Museum\inventory\FactionChestInventory;
 use Steellg0ld\Museum\Plugin;
+use Steellg0ld\Museum\utils\Utils;
 
 class FactionListeners implements Listener
 {
@@ -23,5 +27,20 @@ class FactionListeners implements Listener
     {
         $player = $event->getPlayer();
         if($player instanceof MPlayer) $player->canInteractClaim($event);
+    }
+
+    public function onTap(PlayerInteractEvent $event){
+        if(!$event->getBlock() instanceof Chest) return;
+
+        $player = $event->getPlayer();
+        if(!$player instanceof MPlayer) return;
+        $chunk = $player->getLevel()->getChunkAtPosition($player);
+        if(!Plugin::getInstance()->getClaims()->isInClaim($player->getLevel(), $chunk->getX(),$chunk->getZ())) return;
+        $faction = Plugin::getInstance()->getFactions()->getFactionConfig(Plugin::getInstance()->getClaims()->getFactionClaim($player->getLevel(), $chunk->getX(),$chunk->getZ()));
+        if($player->getFaction()->getIdentifier() == Plugin::getInstance()->getClaims()->getFactionClaim($player->getLevel(), $chunk->getX(),$chunk->getZ())){
+            FactionChestInventory::openInventory($player, $player->getFaction());
+        }else{
+            $player->sendMessage(Utils::createMessage("{PRIMARY}- {SECONDARY}Vous n'avez pas le rôle requis pour accéder au coffre de faction"));
+        }
     }
 }
