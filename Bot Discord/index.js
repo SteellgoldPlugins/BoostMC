f86d4f6c021d795b8cb6950b70a8b3f10c80f353
Gaëtan H <steellgold0@gmail.com>
2021-05-22 22:08:36 +0200

    Add Factions file

--- Bot Discord/index.js
+++ Bot Discord/index.js
@@ -1,14 +1,24 @@
 const Discord = require('discord.js')
 
+const sqlite3 = require('sqlite3').verbose();
+const yaml = require('js-yaml');
+const fs = require('fs');
+
 exports.Version = "v1.0"
+exports.Prefix = "*"
 
 exports.Client = new Discord.Client()
 exports.Client.commands = new Discord.Collection();
 exports.Config = require('./conf/config.json');
 exports.Colors = require('colors');
+exports.Factions = yaml.load(fs.readFileSync('/home/ixti/example.yml', 'utf8'));
 
-const sqlite3 = require('sqlite3').verbose();
-exports.Database = new sqlite3.Database(':memory:');
+exports.Database = new sqlite3.Database('../data.sqlite', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+});
 
 const CommandRegistry = require('./commands/CommandRegister')
 CommandRegistry.registerCommand(exports.Client.commands, "player", true, "commands/minecraft/", "PlayerCommand", exports.Colors)
