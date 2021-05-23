90f68405d41561df26eefbccfc1f744fd02e7a3d
Gaëtan H <steellgold0@gmail.com>
2021-05-23 17:45:32 +0200

    Update tip to message when click barrer

--- Museum OF/src/Steellg0ld/Museum/inventory/FactionChestInventory.php
+++ Museum OF/src/Steellg0ld/Museum/inventory/FactionChestInventory.php
@@ -7,6 +7,7 @@ use muqsit\invmenu\transaction\InvMenuTransaction;
 use muqsit\invmenu\transaction\InvMenuTransactionResult;
 use Steellg0ld\Museum\base\MFaction;
 use Steellg0ld\Museum\base\MPlayer;
+use Steellg0ld\Museum\utils\Utils;
 
 class FactionChestInventory {
     public static function openInventory(MPlayer $player, MFaction $faction){
@@ -14,8 +15,8 @@ class FactionChestInventory {
         $menu->send($player, "Coffre de faction");
         $menu->getInventory()->setContents(unserialize(base64_decode($faction->getChest())));
         $menu->setListener(function(InvMenuTransaction $transaction) use ($player) : InvMenuTransactionResult {
-            if(($transaction->getItemClicked()->getId() . ":".$transaction->getItemClicked()->getDamage()) === "160:15"){
-                $player->sendTip("Vous ne pouvez pas récuperer les vitres de sécuritées");
+            if(($transaction->getItemClicked()->getId() . ":".$transaction->getItemClicked()->getDamage()) === "1000:0"){
+                $player->sendMessage(Utils::createMessage("{ERROR}- {SECONDARY}Vous ne pouvez pas mettre des items dans cette case, améliorer l'amélioration {ERROR}Stockage "));
                 return $transaction->discard();
             }
             return $transaction->continue();
