e13bf4f41ef237f98ba8e32659258958ee00a9b4
Gaëtan H <steellgold0@gmail.com>
2021-05-24 00:34:12 +0200

    Main refactorize

--- Museum OF/src/Steellg0ld/Museum/inventory/FactionChestInventory.php
+++ Museum OF/src/Steellg0ld/Museum/inventory/FactionChestInventory.php
@@ -5,10 +5,8 @@ namespace Steellg0ld\Museum\inventory;
 use muqsit\invmenu\InvMenu;
 use muqsit\invmenu\transaction\InvMenuTransaction;
 use muqsit\invmenu\transaction\InvMenuTransactionResult;
-use pocketmine\entity\Effect;
 use Steellg0ld\Museum\base\MFaction;
 use Steellg0ld\Museum\base\MPlayer;
-use Steellg0ld\Museum\json\data\DataProvider;
 use Steellg0ld\Museum\Plugin;
 use Steellg0ld\Museum\utils\Utils;
 
