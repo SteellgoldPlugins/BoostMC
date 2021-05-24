0425c387b8098bebec96ba60b23fe476bbf793e4
Gaëtan H <steellgold0@gmail.com>
2021-05-24 20:09:50 +0200

    Rank translate to text with lang

--- Museum/src/Steellg0ld/Museum/base/Ranks.php
+++ Museum/src/Steellg0ld/Museum/base/Ranks.php
@@ -2,8 +2,15 @@
 
 namespace Steellg0ld\Museum\base;
 
+use Steellg0ld\Museum\Plugin;
+
 class Ranks {
     CONST PLAYER = 0;
     CONST VIP = 1;
     CONST STAFF = 2;
+
+    public static function translate(Player $player, Int $rank){
+        $config = Plugin::getInstance()->getMessages($player->getLang());
+        return $config->exists("RANK_".$rank) ? $config->get("RANK_".$rank) : $config->get("NO_RANK_FOUND");
+    }
 }
