089454fa354cb40d85a47417278887303f5e2524
Gaëtan H <steellgold0@gmail.com>
2021-05-24 16:53:41 +0200

    custom sendMessage function

--- Museum/src/Steellg0ld/Museum/utils/Utils.php
+++ Museum/src/Steellg0ld/Museum/utils/Utils.php
@@ -2,7 +2,19 @@
 
 namespace Steellg0ld\Museum\utils;
 
+use Steellg0ld\Museum\base\Player;
+use Steellg0ld\Museum\Plugin;
+
 class Utils {
-    CONST PREFIX_BASIC = "";
-    CONST PREFIX_ERROR = "";
+    CONST SEARCH = ["{PREFIX}", "{PRIMARY}", "{SECONDARY}"];
+    CONST REPLACE = ["§l» §r§f", "§a", "§f"];
+
+    public static function getPrefix(): string{
+        return "§l» §r§f";
+    }
+
+    public static function sendMessage(Player $player, $message = "NO_MESSAGE_FOUND", $find = [], $replace = [])
+    {
+        $player->sendMessage(str_replace($find, $replace, Plugin::getInstance()->getMessages($player->getLang())->exists($message) ? str_replace(self::SEARCH, self::REPLACE, Plugin::getInstance()->getMessages($player->getLang())->get($message)) : "NO_MESSAGE_FOUND"));
+    }
 }
