07f4e771a5b84edabfc92fdee02b5c987be56087
Gaëtan H <steellgold0@gmail.com>
2021-05-24 14:29:06 +0200

    PocketMine Updated PATH

--- PocketMine-MP/src/pocketmine/network/upnp/UPnP.php
+++ PocketMine-MP/src/pocketmine/network/upnp/UPnP.php
@@ -22,7 +22,7 @@
 // This code is based on a Go implementation and its license is below:
 // Copyright (c) 2010 Jack Palevich. All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without
+// Redistribution and use in source and binary FormAPI, with or without
 // modification, are permitted provided that the following conditions are
 // met:
 //
