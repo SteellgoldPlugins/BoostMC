a5bd9e3d7e86cbeb90465c4cc92105d1548d25ee
Gaëtan H <steellgold0@gmail.com>
2021-05-24 20:16:24 +0200

    Translate sendMessage, and getMessage

--- Museum/src/Steellg0ld/Museum/utils/Utils.php
+++ Museum/src/Steellg0ld/Museum/utils/Utils.php
@@ -6,15 +6,18 @@ use Steellg0ld\Museum\base\Player;
 use Steellg0ld\Museum\Plugin;
 
 class Utils {
-    CONST SEARCH = ["{PREFIX}", "{PRIMARY}", "{SECONDARY}"];
-    CONST REPLACE = ["§l» §r§f", "§a", "§f"];
+    CONST SEARCH = ["{PREFIX}", "{PRIMARY}", "{SECONDARY}", "{ERROR}", "{DARK_ERROR}"];
+    CONST REPLACE = ["§l» §r§f", "§a", "§f", "§c", "§4"];
 
     public static function getPrefix(): string{
         return "§l» §r§f";
     }
 
-    public static function sendMessage(Player $player, $message = "NO_MESSAGE_FOUND", $find = [], $replace = [])
-    {
-        $player->sendMessage(str_replace($find, $replace, Plugin::getInstance()->getMessages($player->getLang())->exists($message) ? str_replace(self::SEARCH, self::REPLACE, Plugin::getInstance()->getMessages($player->getLang())->get($message)) : "NO_MESSAGE_FOUND"));
+    public static function sendMessage(Player $player, String $message = "NO_MESSAGE_FOUND", array $search = [], array $replace = []){
+        $player->sendMessage(str_replace($search, $replace,Plugin::getInstance()->getMessages($player->getLang())->exists($message) ? str_replace(self::SEARCH, self::REPLACE, Plugin::getInstance()->getMessages($player->getLang())->get($message)) : "NO_MESSAGE_FOUND"));
+    }
+
+    public static function getMessage(Player $player, String $message = "NO_MESSAGE_FOUND", array $search = [], array $replace = []){
+        return str_replace($search, $replace,Plugin::getInstance()->getMessages($player->getLang())->exists($message) ? str_replace(self::SEARCH, self::REPLACE, Plugin::getInstance()->getMessages($player->getLang())->get($message)) : "NO_MESSAGE_FOUND");
     }
 }
