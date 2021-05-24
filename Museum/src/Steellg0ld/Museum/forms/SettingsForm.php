a1e700ea22b9aeadc945f841603a1840cf690efc
Gaëtan H <steellgold0@gmail.com>
2021-05-24 20:12:53 +0200

    Add settings form (non-fictive for lang)

--- Museum/src/Steellg0ld/Museum/forms/SettingsForm.php
+++ Museum/src/Steellg0ld/Museum/forms/SettingsForm.php
@@ -2,6 +2,44 @@
 
 namespace Steellg0ld\Museum\forms;
 
-class SettingsForm{
+use FormAPI\CustomForm;
+use Steellg0ld\Museum\base\Player;
+use Steellg0ld\Museum\base\Ranks;
+use Steellg0ld\Museum\utils\Utils;
 
+class SettingsForm{
+    public static function openForm(Player $player)
+    {
+        {
+            $form = new CustomForm(
+                function (Player $p, $data) {
+                    if ($data !== null) {
+                        switch ($data[2]){
+                            case 0:
+                                $p->lang = "fr_FR";
+                                break;
+                            case 1:
+                                $p->lang = "en_EN";
+                                break;
+                            case 2:
+                                $p->lang = "es_ES";
+                                break;
+                            case 3:
+                                $p->lang = "it_IT";
+                                break;
+                            case 4:
+                                $p->lang = "ch_CH";
+                                break;
+                        }
+                    }
+                }
+            );
+
+            $form->setTitle(Utils::getMessage($player, "SETTINGS_TITLE_FORM"));
+            $form->addLabel(Utils::getMessage($player, "SETTINGS_LABEL_FORM", ["{RANK}"], [Ranks::translate($player,$player->getRank())]));
+            $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_0"),["$","€","@"]);
+            $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_1"),[Utils::getMessage($player, "SETTINGS_DROPDOWN_1_0"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_1"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_2"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_3"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_4")]);
+            $player->sendForm($form);
+        }
+    }
 }
