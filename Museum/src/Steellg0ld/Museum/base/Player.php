cccc898d7de537975b19b986e7b720ffbabff250
Gaëtan H <steellgold0@gmail.com>
2021-05-24 16:53:21 +0200

    Add lang, notifications in player class

--- Museum/src/Steellg0ld/Museum/base/Player.php
+++ Museum/src/Steellg0ld/Museum/base/Player.php
@@ -4,5 +4,27 @@ namespace Steellg0ld\Museum\base;
 
 class Player extends \pocketmine\Player
 {
+    public int $rank = Ranks::PLAYER;
+    public array $notifications = [
+        "player_status" => 1
+    ];
 
+    public $lang = "fr_FR";
+
+    public function hasRank(Int ...$ranks): bool{
+        if(in_array($this->rank, $ranks)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function register() {
+        var_dump("register");
+        $this->rank = Ranks::PLAYER;
+    }
+
+    public function getLang(): string{
+        return $this->lang;
+    }
 }
