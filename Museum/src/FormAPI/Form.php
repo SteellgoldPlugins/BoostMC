08b00e86da82292bee4195144a40d06cf526422e
Gaëtan H <steellgold0@gmail.com>
2021-05-24 14:28:04 +0200

    Add Form API

---
+++ Museum/src/FormAPI/Form.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace FormAPI;
+
+use pocketmine\form\Form as IForm;
+use pocketmine\Player;
+
+abstract class Form implements IForm
+{
+    const FACTION_TITLE = "Faction";
+    const FACTION_UPGRADE_TITLE = "Amélioration: ";
+    const CODE_TITLE = "Code";
+    const ECONOMY_TITLE = "Economie";
+
+    /** @var array */
+    protected $data = [];
+    /** @var callable|null */
+    private $callable;
+
+    /**
+     * @param callable|null $callable
+     */
+    public function __construct(?callable $callable)
+    {
+        $this->callable = $callable;
+    }
+
+    /**
+     * @param Player $player
+     * @see Player::sendForm()
+     *
+     * @deprecated
+     */
+    public function sendToPlayer(Player $player): void
+    {
+        $player->sendForm($this);
+    }
+
+    public function handleResponse(Player $player, $data): void
+    {
+        $this->processData($data);
+        $callable = $this->getCallable();
+        if ($callable !== null) {
+            $callable($player, $data);
+        }
+    }
+
+    public function processData(&$data): void
+    {
+    }
+
+    public function getCallable(): ?callable
+    {
+        return $this->callable;
+    }
+
+    public function setCallable(?callable $callable)
+    {
+        $this->callable = $callable;
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->data;
+    }
+}
