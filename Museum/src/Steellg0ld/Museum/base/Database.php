f3c476595dd7f7d8daba37c75b4a1354f7da8cec
Gaëtan H <steellgold0@gmail.com>
2021-05-24 20:07:34 +0200

    Intitialize database

--- Museum/src/Steellg0ld/Museum/base/Database.php
+++ Museum/src/Steellg0ld/Museum/base/Database.php
@@ -9,4 +9,9 @@ class Database
     public function getDatabase(): \SQLite3{
         return new \SQLite3(Plugin::getInstance()->getDataFolder() . Plugin::FILE_DB);
     }
+
+    public function initialize(){
+        Plugin::getInstance()->getDatabase()->getDatabase()->query("CREATE TABLE IF NOT EXISTS faction (faction VARCHAR(255) PRIMARY KEY, players TEXT, power int, money int, allies TEXT, claims TEXT, chest TEXT);");
+        Plugin::getInstance()->getDatabase()->getDatabase()->query("CREATE TABLE IF NOT EXISTS players (player VARCHAR(255) PRIMARY KEY, address TEXT, faction TEXT, role int, rank int, money int, lang TEXT, settings TEXT);");
+    }
 }
