0a28ab066d80ebfecb9d4f189bfa1f533d6e5dc3
Gaëtan H <steellgold0@gmail.com>
2021-05-24 20:16:11 +0200

    Database load factions to RAM

---
+++ Museum/src/Steellg0ld/Museum/tasks/async/LoadDatabase.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Steellg0ld\Museum\tasks\async;
+
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
+use Steellg0ld\Museum\base\Faction;
+use Steellg0ld\Museum\Plugin;
+
+class LoadDatabase extends AsyncTask {
+
+    private $db;
+
+    public function __construct() {
+        $this->db = array(Plugin::getInstance()->getDataFolder() . Plugin::FILE_DB);
+    }
+
+    public function onRun() {
+        $results = [];
+
+        $db = new \SQLite3($this->db[0]);
+        $data = $db->query("SELECT faction, players, power, money, allies, claims FROM faction");
+        while ($resultArr = $data->fetchArray(SQLITE3_ASSOC)) {
+            $results["faction"][$resultArr['faction']] = array(unserialize(base64_decode($resultArr['players'])), $resultArr['power'], $resultArr['money'], unserialize(base64_decode($resultArr['allies'])), unserialize(base64_decode($resultArr['claims'])));
+        }
+        $this->setResult($results);
+    }
+
+    /**
+     * @param Server $server
+     */
+    public function onCompletion(Server $server) {
+        $result = $this->getResult();
+
+        if(!empty($result)) {
+            if (isset($result["faction"])) {
+                foreach ($result["faction"] as $key => $array) {
+                    Faction::$factions[$key] = array("players" => array_unique($array[0]), "power" => $array[1], "money" => $array[2], "allies" => $array[3]);
+                    Faction::$claims[$key] = $array[4];
+                }
+            }
+        }
+    }
+}
