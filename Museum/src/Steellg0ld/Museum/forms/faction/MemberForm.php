2d235f4910674c33fe915aace63e6ff2367b8ff0
Gaëtan H <steellgold0@gmail.com>
2021-05-25 20:32:32 +0200

    Member form

--- Museum/src/Steellg0ld/Museum/forms/faction/MemberForm.php
+++ Museum/src/Steellg0ld/Museum/forms/faction/MemberForm.php
@@ -15,7 +15,7 @@ class MemberForm{
             $form = new SimpleForm(
                 function (Player $p, $data) {
                     if ($data !== null) {
-
+                        self::member($p);
                     }
                 }
             );
@@ -28,6 +28,23 @@ class MemberForm{
             $form->addButton("Joueur 4\nChef",0,"textures/ui/offline");
             // $form->addButton(Utils::getMessage($player, "FACTION_MANAGE_INVITE_BUTTON_FORM"),0,"textures/ui/invite_base");
             // $form->addButton(Utils::getMessage($player, "FACTION_MANAGE_MEMBER_BUTTON_FORM"),0,"textures/ui/icon_setting");
+            $player->sendForm($form);
+        }
+    }
+
+    public static function member(Player $player)
+    {
+        {
+            $form = new SimpleForm(
+                function (Player $p, $data) {
+                    if ($data !== null) {
+
+                    }
+                }
+            );
+
+            $form->setTitle(Utils::getMessage($player, "FACTION_MANAGE_MEMBERS_BUTTON_FORM"));
+            $form->setContent(Utils::getMessage($player, "FACTION_MANAGE_MEMBER_LABEL_FORM", ["{NAME}", "{ROLE}", "{JOIN_DATE}", "{MONEY_INVEST}", "{LAST_CONNECT}", "{KILLS}", "{DEATHS}"],["Joueur","Chef","25 décembre 2020", 5620,"12 mai 2021", 6, 1]));
             $player->sendForm($form);
         }
     }
