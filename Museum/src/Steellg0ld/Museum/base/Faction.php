2de6cc583fc00add101d2f5d0a00bdd2b0701971
Gaëtan H <steellgold0@gmail.com>
2021-05-26 18:57:41 +0200

    Default power -> 20, and create faction function

--- Museum/src/Steellg0ld/Museum/base/Faction.php
+++ Museum/src/Steellg0ld/Museum/base/Faction.php
@@ -2,9 +2,12 @@
 
 namespace Steellg0ld\Museum\base;
 
+use pocketmine\Server;
 use Steellg0ld\Museum\Plugin;
+use Steellg0ld\Museum\utils\Utils;
 
-class Faction{
+class Faction
+{
     public static array $factions = [];
     public static array $claims = [];
 
@@ -13,8 +16,18 @@ class Faction{
     public const OFFICIER = 2;
     public const LEADER = 3;
 
-    public const DEFAULT_POWER = 0;
+    public const DEFAULT_POWER = 20;
     public const POWER_PER_KILL = 5;
     public const POWER_PER_DEATHS = 10;
     public const INVITATION_EXPIRATION_TIME = 60;
+
+    public static function create(Player $player, string $faction): void
+    {
+        self::$factions[$faction] = array("players" => array($player->getName()), "power" => self::DEFAULT_POWER, "money" => 0, "allies" => array());
+        self::$claims[$faction] = array();
+        $player->faction = $faction;
+        $player->faction_role = self::LEADER;
+
+        $player->sendMessage(Utils::getMessage($player, "FACTION_CREATED", ["{FACTION}"],[$faction]));
+    }
 }
