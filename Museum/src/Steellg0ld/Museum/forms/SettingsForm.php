62aac19cbf89aa7ca4d5e9f6e5050b5a857e226e
Gaëtan H <steellgold0@gmail.com>
2021-05-27 20:47:12 +0200

    Optimize settings form and send success message

--- Museum/src/Steellg0ld/Museum/forms/SettingsForm.php
+++ Museum/src/Steellg0ld/Museum/forms/SettingsForm.php
@@ -3,6 +3,7 @@
 namespace Steellg0ld\Museum\forms;
 
 use FormAPI\CustomForm;
+use Steellg0ld\Museum\base\Economy;
 use Steellg0ld\Museum\base\Player;
 use Steellg0ld\Museum\base\Ranks;
 use Steellg0ld\Museum\utils\Utils;
@@ -14,31 +15,17 @@ class SettingsForm{
             $form = new CustomForm(
                 function (Player $p, $data) {
                     if ($data !== null) {
-                        switch ($data[2]){
-                            case 0:
-                                $p->lang = "fr_FR";
-                                break;
-                            case 1:
-                                $p->lang = "en_EN";
-                                break;
-                            case 2:
-                                $p->lang = "es_ES";
-                                break;
-                            case 3:
-                                $p->lang = "it_IT";
-                                break;
-                            case 4:
-                                $p->lang = "ch_CH";
-                                break;
-                        }
+                        $p->lang = Utils::LANGS[$data[2]];
+                        $p->settings["economy_symbol"] = $data[1] + 1;
+                        Utils::sendMessage($p, "SETTINGS_UPDATED");
                     }
                 }
             );
 
             $form->setTitle(Utils::getMessage($player, "SETTINGS_TITLE_FORM"));
             $form->addLabel(Utils::getMessage($player, "SETTINGS_LABEL_FORM", ["{RANK}"], [Ranks::translate($player,$player->getRank())]));
-            $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_0"),["$","€","@"]);
-            $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_1"),[Utils::getMessage($player, "SETTINGS_DROPDOWN_1_0"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_1"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_2"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_3"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_4")]);
+            $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_0"),[Economy::DOL,Economy::EUR,Economy::ARO,Economy::YEN]);
+            $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_1"),[Utils::getMessage($player, "SETTINGS_DROPDOWN_1_0"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_1"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_2"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_3"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_4")],array_search($player->lang,Utils::LANGS));
             $player->sendForm($form);
         }
     }
