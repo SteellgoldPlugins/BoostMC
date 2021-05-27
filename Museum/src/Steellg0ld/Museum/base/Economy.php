b8fd370be034e8587b5906c3eb3922edce893b31
Gaëtan H <steellgold0@gmail.com>
2021-05-27 20:46:02 +0200

    Add Economy Basic Functions

--- Museum/src/Steellg0ld/Museum/base/Economy.php
+++ Museum/src/Steellg0ld/Museum/base/Economy.php
@@ -4,5 +4,32 @@ namespace Steellg0ld\Museum\base;
 
 class Economy
 {
+    public const SYMBOLS = [
+        1 => self::DOL,
+        2 => self::EUR,
+        3 => self::ARO,
+        4 => self::YEN
+    ];
 
+    const DOL = "$";
+    const EUR = "€";
+    const ARO = "@";
+    const YEN = "¥";
+
+    public function addMoney(Player $player, Int $count){
+        $player->money = $player->money + $count;
+    }
+
+    public function delMoney(Player $player, Int $count): bool {
+        if($this->haveNeededMoney($player, $count)) return $this->reduceMoney($player, $count); else return false;
+    }
+
+    public function haveNeededMoney(Player $player, Int $need): bool {
+        return $player->money >= $need;
+    }
+
+    private function reduceMoney(Player $player, Int $count): bool {
+        $player->money = $player->money - $count;
+        return true;
+    }
 }
