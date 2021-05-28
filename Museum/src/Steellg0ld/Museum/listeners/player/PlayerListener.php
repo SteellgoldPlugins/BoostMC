a3cfff94db69f9e056512f7cf481fa61962a1eb4
Gaëtan H <steellgold0@gmail.com>
2021-05-28 21:45:58 +0200

    Add rank chat interact

--- Museum/src/Steellg0ld/Museum/listeners/player/PlayerListener.php
+++ Museum/src/Steellg0ld/Museum/listeners/player/PlayerListener.php
@@ -3,17 +3,21 @@
 namespace Steellg0ld\Museum\listeners\player;
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerCreationEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\Server;
 use Steellg0ld\Museum\api\VPN;
 use Steellg0ld\Museum\base\Player;
+use Steellg0ld\Museum\base\Ranks;
 use Steellg0ld\Museum\Plugin;
 use Steellg0ld\Museum\utils\Utils;
 
 class PlayerListener implements Listener
 {
+    public const CHAT = "{PRIMARY}[{SECONDARY}{RANK}{PRIMARY}] {PRIMARY}[{SECONDARY}{FACTION}{FACTION_RANK}{PRIMARY}] {SECONDARY}{PLAYER_NAME} {PRIMARY}§l» §r{PRIMARY}{MESSAGE}";
+
     public function create(PlayerCreationEvent $event) {
         $event->setPlayerClass(Player::class);
     }
@@ -50,6 +54,15 @@ class PlayerListener implements Listener
                     if ($players->settings["player_status"]) Utils::sendMessage($players, "PLAYER_LEAVE", ["{PLAYER}"], [$player->getName()]);
                 }
             }
+        }
+    }
+
+    public function onChat(PlayerChatEvent $event){
+        $event->setCancelled();
+        $p = $event->getPlayer();
+        if(!$p instanceof Player) return;
+        foreach (Server::getInstance()->getOnlinePlayers() as $player){
+            if($player instanceof Player) $player->sendMessage(str_replace(["{PRIMARY}", "{SECONDARY}", "{RANK}", "{FACTION}", "{FACTION_RANK}", "{PLAYER_NAME}", "{MESSAGE}"],[Ranks::$ranks[$p->getRank()]["p"], Ranks::$ranks[$p->getRank()]["s"], Ranks::$ranks[$p->getRank()]["r"], $p->getFaction(),"***",$p->getName(),$event->getMessage()],self::CHAT));
         }
     }
 }
