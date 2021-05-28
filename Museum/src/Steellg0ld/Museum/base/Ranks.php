0e1bc91d081b2093abf2c17ad8242280afe014f6
Gaëtan H <steellgold0@gmail.com>
2021-05-28 21:44:29 +0200

    add constant to formats ranks

--- Museum/src/Steellg0ld/Museum/base/Ranks.php
+++ Museum/src/Steellg0ld/Museum/base/Ranks.php
@@ -10,6 +10,12 @@ class Ranks {
     CONST VIP_PLUS = 2;
     CONST STAFF = 3;
 
+    public static array $ranks = [
+        0 => ["p" => "§f", "s" => "§7", "r" => "J"],
+        1 => ["p" => "§g", "s" => "§e", "r" => "V"],
+        2 => ["p" => "§2", "s" => "§a", "r" => "V+"],
+        3 => ["p" => "§4", "s" => "§c", "r" => "S"]
+    ];
 
     public static function translate(Player $player, Int $rank){
         $config = Plugin::getInstance()->getMessages($player->getLang());
