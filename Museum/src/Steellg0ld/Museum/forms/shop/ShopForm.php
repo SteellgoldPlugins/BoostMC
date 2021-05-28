1de04ea731555c86f11ca27249cae6785d55cc5a
Gaëtan H <steellgold0@gmail.com>
2021-05-28 21:47:12 +0200

    Add ShopForm

---
+++ Museum/src/Steellg0ld/Museum/forms/shop/ShopForm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Steellg0ld\Museum\forms\shop;
+
+use FormAPI\CustomForm;
+use FormAPI\SimpleForm;
+use pocketmine\item\Item;
+use pocketmine\item\ItemIds;
+use Steellg0ld\Museum\base\Economy;
+use Steellg0ld\Museum\base\Player;
+use Steellg0ld\Museum\base\Ranks;
+use Steellg0ld\Museum\Plugin;
+use Steellg0ld\Museum\utils\Prices;
+use Steellg0ld\Museum\utils\Utils;
+
+class ShopForm{
+}
