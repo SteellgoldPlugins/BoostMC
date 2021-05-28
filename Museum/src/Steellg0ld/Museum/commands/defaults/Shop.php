7a5218b1cde1ff3f8ddfbbe8fab4852b9a19f137
Gaëtan H <steellgold0@gmail.com>
2021-05-28 21:45:29 +0200

    ShopCommand

---
+++ Museum/src/Steellg0ld/Museum/commands/defaults/Shop.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Steellg0ld\Museum\commands\defaults;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use Steellg0ld\Museum\base\Player;
+use Steellg0ld\Museum\forms\FactionForm;
+use Steellg0ld\Museum\forms\SettingsForm;
+use Steellg0ld\Museum\forms\shop\ShopForm;
+
+class Shop extends Command {
+    public function __construct(string $name, string $description = "", string $usageMessage = null, array $aliases = [])
+    {
+        parent::__construct($name, $description, $usageMessage, $aliases);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($sender instanceof Player){
+            ShopForm::open($sender);
+        }
+    }
+}
