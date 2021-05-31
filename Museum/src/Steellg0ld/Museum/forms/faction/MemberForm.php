120a30cce034fabf27fe9b4afb1cbd11a38c4c11
Gaëtan H <steellgold0@gmail.com>
2021-05-31 21:03:12 +0200

    add invite form

--- Museum/src/Steellg0ld/Museum/forms/faction/MemberForm.php
+++ Museum/src/Steellg0ld/Museum/forms/faction/MemberForm.php
@@ -42,11 +42,47 @@ class MemberForm{
         }
     }
 
-    public static function member(Player $player)
+    public static function invite(Player $player)
+    {
+        {
+            $form = new CustomForm(
+                function (Player $p, $data) {
+                    if ($data !== null) {
+                        $player = Server::getInstance()->getPlayer($data[1]);
+                        if($player instanceof Player){
+                            if($player->hasFaction()){
+                                Faction::sendInvitation($player,$p->getFaction());
+                                Utils::sendMessage($p,"FACTION_INVITE", ["{INVITED}"], [$player->getName()]);
+                                Utils::sendMessage($player,"FACTION_INVITED",[
+                                    "{OFFICIER}",
+                                    "{FACTION}"
+                                ],[
+                                    $p->getName(),
+                                    $p->getFaction()
+                                ]);
+                            }else{
+                                Utils::sendMessage($p,"FACTION_PLAYER_ALREADY_FACTION", ["{PLAYER}", "{FACTION}"], [$player->getName(), $player->getFaction()]);
+                            }
+                        }else{
+                            Utils::sendMessage($p, "FACTION_INVITE_PLAYER_DISCONNECTED");
+                        }
+                    }
+                }
+            );
+
+            $form->setTitle(Utils::getMessage($player, "FACTION_MANAGE_MEMBERS_BUTTON_FORM"));
+            $form->addLabel(Utils::getMessage($player, "FACTION_INVITE_LABEL_FORM"));
+            $form->addInput(Utils::getMessage($player,"FACTION_INVITE_INPUT_FORM"),0);
+            $form->addDropdown(Utils::getMessage($player, "FACTION_MANAGE_MEMBERS_LABEL_FORM"),["Recrue", "Membre", "Officier"],0);
+            $player->sendForm($form);
+        }
+    }
+
+    public static function member(Player $player, String $name)
     {
         {
             $form = new SimpleForm(
-                function (Player $p, $data) {
+                function (Player $p, $data) use ($name) {
                     if ($data !== null) {
 
                     }
@@ -54,7 +90,7 @@ class MemberForm{
             );
 
             $form->setTitle(Utils::getMessage($player, "FACTION_MANAGE_MEMBERS_BUTTON_FORM"));
-            $form->setContent(Utils::getMessage($player, "FACTION_MANAGE_MEMBER_LABEL_FORM", ["{NAME}", "{ROLE}", "{JOIN_DATE}", "{MONEY_INVEST}", "{LAST_CONNECT}", "{KILLS}", "{DEATHS}"],["Joueur","Chef","25 décembre 2020", 5620,"12 mai 2021", 6, 1]));
+            $form->setContent(Utils::getMessage($player, "FACTION_MANAGE_MEMBER_LABEL_FORM", ["{NAME}", "{ROLE}", "{JOIN_DATE}", "{MONEY_INVEST}", "{LAST_CONNECT}", "{KILLS}", "{DEATHS}"],[$name,"","25 décembre 2020",5620,"12 mai 2021", 6, 1]));
             $player->sendForm($form);
         }
     }
