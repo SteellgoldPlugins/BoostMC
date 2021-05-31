ab5675024278ad6e8299358a60333c32c265f087
Gaëtan H <steellgold0@gmail.com>
2021-05-31 21:03:05 +0200

    update members list

--- Museum/src/Steellg0ld/Museum/forms/faction/MemberForm.php
+++ Museum/src/Steellg0ld/Museum/forms/faction/MemberForm.php
@@ -4,6 +4,9 @@ namespace Steellg0ld\Museum\forms\faction;
 
 use FormAPI\CustomForm;
 use FormAPI\SimpleForm;
+use pocketmine\item\Item;
+use pocketmine\Server;
+use Steellg0ld\Museum\base\Faction;
 use Steellg0ld\Museum\base\Player;
 use Steellg0ld\Museum\base\Ranks;
 use Steellg0ld\Museum\utils\Utils;
@@ -12,22 +15,29 @@ class MemberForm{
     public static function list(Player $player)
     {
         {
+            $array = [];
+            foreach (Faction::getMembers($player->getFaction()) as $member) {
+                array_push($array, $member);
+            }
+
             $form = new SimpleForm(
-                function (Player $p, $data) {
+                function (Player $p, $data) use ($array) {
                     if ($data !== null) {
-                        self::member($p);
+                        if($data == 0){
+                            self::invite($p);
+                        }else{
+                            self::member($p, $array[$data]);
+                        }
                     }
                 }
             );
 
             $form->setTitle(Utils::getMessage($player, "FACTION_MANAGE_MEMBERS_BUTTON_FORM"));
             $form->setContent(Utils::getMessage($player, "FACTION_MANAGE_MEMBERS_LABEL_FORM"));
-            $form->addButton("Joueur 1\nMembre",0,"textures/ui/online");
-            $form->addButton("Joueur 2\nMembre",0,"textures/ui/offline");
-            $form->addButton("Joueur 3\nOfficier",0,"textures/ui/offline");
-            $form->addButton("Joueur 4\nChef",0,"textures/ui/offline");
-            // $form->addButton(Utils::getMessage($player, "FACTION_MANAGE_INVITE_BUTTON_FORM"),0,"textures/ui/invite_base");
-            // $form->addButton(Utils::getMessage($player, "FACTION_MANAGE_MEMBER_BUTTON_FORM"),0,"textures/ui/icon_setting");
+            $form->addButton(Utils::getMessage($player, "FACTION_MANAGE_INVITE_BUTTON_FORM"));
+            foreach ($array as $member) {
+                $form->addButton($member);
+            }
             $player->sendForm($form);
         }
     }
