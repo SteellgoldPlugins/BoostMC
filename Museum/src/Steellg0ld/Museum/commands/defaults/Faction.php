4135be510537d1141d9e3c64a963fb611cfb82d7
Gaëtan H <steellgold0@gmail.com>
2021-05-31 21:02:04 +0200

    Faction command, accept and deny

--- Museum/src/Steellg0ld/Museum/commands/defaults/Faction.php
+++ Museum/src/Steellg0ld/Museum/commands/defaults/Faction.php
@@ -4,9 +4,12 @@ namespace Steellg0ld\Museum\commands\defaults;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\Server;
 use Steellg0ld\Museum\base\Player;
+use Steellg0ld\Museum\forms\faction\MemberForm;
 use Steellg0ld\Museum\forms\FactionForm;
-use Steellg0ld\Museum\forms\SettingsForm;
+use Steellg0ld\Museum\utils\Utils;
+use Steellg0ld\Museum\base\Faction as FactionAPI;
 
 class Faction extends Command {
     public function __construct(string $name, string $description = "", string $usageMessage = null, array $aliases = [])
@@ -17,10 +20,26 @@ class Faction extends Command {
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
         if($sender instanceof Player){
-            if($sender->hasFaction()){
-                FactionForm::createForm($sender);
-            }else{
-                FactionForm::manage($sender);
+            if(isset($args[0])){
+                switch ($args[0]){
+                    case "create":
+                        if($sender->hasFaction()){
+                            FactionForm::createForm($sender);
+                        }else{
+                            FactionForm::manage($sender);
+                        }
+                        break;
+                    case "invite":
+                        MemberForm::invite($sender);
+                        break;
+                    case "accept":
+                        var_dump(FactionAPI::$invitations[$sender->getName()]);
+                        FactionAPI::acceptInvitation($sender);
+                        break;
+                    case "deny":
+                        FactionAPI::denyInvitation($sender);
+                        break;
+                }
             }
         }
     }
