c280ef8cd0f8a9fe90900fe05bdf43054c021743
Gaëtan H <steellgold0@gmail.com>
2021-05-31 21:01:03 +0200

    Invite faction feature

--- Museum/src/Steellg0ld/Museum/base/Faction.php
+++ Museum/src/Steellg0ld/Museum/base/Faction.php
@@ -10,11 +10,20 @@ class Faction
 {
     public static array $factions = [];
     public static array $claims = [];
+    public static array $invitations = [];
+    public static array $invitationsTimeout = [];
 
     public const RECRUE = 0;
     public const MEMBER = 1;
     public const OFFICIER = 2;
     public const LEADER = 3;
+
+    public const ROLES = [
+        0 => "RECRUE",
+        1 => "MEMBER",
+        2 => "OFFICIER",
+        3 => "LEADER"
+    ];
 
     public const DEFAULT_POWER = 20;
     public const DEFAULT_FACTION_PRICE = 500;
@@ -22,10 +31,11 @@ class Faction
     public const POWER_PER_DEATHS = 10;
     public const INVITATION_EXPIRATION_TIME = 60;
     public const DEFAULT_ECONOMY_SYMBOL = 1;
+    public const DEFAULT_MAX_MEMBERS = 10;
 
     public static function create(Player $player, string $faction, string $description, string $claim_message): void
     {
-        self::$factions[$faction] = array("players" => array($player->getName()), "power" => self::DEFAULT_POWER, "money" => 0, "allies" => array(), "description" => $description, "claim_message" => $claim_message);
+        self::$factions[$faction] = array("players" => array($player->getName()), "power" => self::DEFAULT_POWER, "money" => 0, "allies" => array(), "description" => $description, "claim_message" => $claim_message, "roles" => array($player->getName() => 3), "dates" => array($player->getName() => time()), "invests" => array($player->getName() => 0));
         self::$claims[$faction] = array();
 
         $player->faction = $faction;
