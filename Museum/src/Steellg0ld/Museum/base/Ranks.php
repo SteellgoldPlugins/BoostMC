d9e04437fb10d5e45c764e03284f5f2dbff39460
Gaëtan H <steellgold0@gmail.com>
2021-06-04 17:01:31 +0200

    add same ranks

--- Museum/src/Steellg0ld/Museum/base/Ranks.php
+++ Museum/src/Steellg0ld/Museum/base/Ranks.php
@@ -8,13 +8,20 @@ class Ranks {
     CONST PLAYER = 0;
     CONST VIP = 1;
     CONST VIP_PLUS = 2;
-    CONST STAFF = 3;
+    CONST YOUTUBE = 3;
+    CONST TWITCH = 4;
+    CONST STAFF = 5;
+    CONST ADMIN = 6;
+    CONST RANKS = ["Joueur", "VIP", "VIP+", "Youtube", "Twitch", "Staff", "Administrateur"];
 
     public static array $ranks = [
-        0 => ["p" => "§f", "s" => "§7", "r" => "J"],
-        1 => ["p" => "§g", "s" => "§e", "r" => "V"],
-        2 => ["p" => "§2", "s" => "§a", "r" => "V+"],
-        3 => ["p" => "§4", "s" => "§c", "r" => "S"]
+        0 => ["p" => "§f", "s" => "§c", "r" => "J"],
+        1 => ["p" => "§f", "s" => "§c", "r" => "V"],
+        2 => ["p" => "§f", "s" => "§c", "r" => "V+"],
+        3 => ["p" => "§f", "s" => "§c", "r" => "YT"],
+        4 => ["p" => "§f", "s" => "§c", "r" => "T"],
+        5 => ["p" => "§f", "s" => "§c", "r" => "S"],
+        6 => ["p" => "§f", "s" => "§c", "r" => "A"]
     ];
 
     public static function translate(Player $player, Int $rank){
