711667d2b2345eb66f3089381605972a983817d0
Gaëtan H <steellgold0@gmail.com>
2021-06-07 00:45:15 +0200

    Emoticons command (Temporary)

---
+++ Museum/src/Steellg0ld/Museum/commands/defaults/Test.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Steellg0ld\Museum\commands\defaults;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+use Steellg0ld\Museum\base\Player;
+use Steellg0ld\Museum\forms\Emoticons;
+use Steellg0ld\Museum\forms\faction\MemberForm;
+use Steellg0ld\Museum\forms\FactionForm;
+use Steellg0ld\Museum\utils\Utils;
+use Steellg0ld\Museum\base\Faction as FactionAPI;
+
+class Test extends Command {
+    public function __construct(string $name, string $description = "", string $usageMessage = null, array $aliases = [])
+    {
+        parent::__construct($name, $description, $usageMessage, $aliases);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($sender instanceof Player){
+            Emoticons::manage($sender);
+        }
+    }
+}
