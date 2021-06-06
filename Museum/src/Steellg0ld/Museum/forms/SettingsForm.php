9c6726c4e27600c60822a81d9f86a6a6eecd106c
Gaëtan H <steellgold0@gmail.com>
2021-06-07 00:45:53 +0200

    Add wrench icon to settings forms

--- Museum/src/Steellg0ld/Museum/forms/SettingsForm.php
+++ Museum/src/Steellg0ld/Museum/forms/SettingsForm.php
@@ -6,6 +6,7 @@ use FormAPI\CustomForm;
 use Steellg0ld\Museum\base\Economy;
 use Steellg0ld\Museum\base\Player;
 use Steellg0ld\Museum\base\Ranks;
+use Steellg0ld\Museum\utils\Unicode;
 use Steellg0ld\Museum\utils\Utils;
 
 class SettingsForm{
@@ -22,7 +23,7 @@ class SettingsForm{
                 }
             );
 
-            $form->setTitle(Utils::getMessage($player, "SETTINGS_TITLE_FORM"));
+            $form->setTitle(Unicode::SETTINGS . " " . Utils::getMessage($player, "SETTINGS_TITLE_FORM"));
             $form->addLabel(Utils::getMessage($player, "SETTINGS_LABEL_FORM", ["{RANK}"], [Ranks::translate($player,$player->getRank())]));
             $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_0"),[Economy::DOL,Economy::EUR,Economy::ARO,Economy::YEN]);
             $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_1"),[Utils::getMessage($player, "SETTINGS_DROPDOWN_1_0"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_1"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_2"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_3"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_4")],array_search($player->lang,Utils::LANGS));
