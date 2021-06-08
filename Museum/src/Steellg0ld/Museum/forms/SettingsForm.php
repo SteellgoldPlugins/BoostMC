a8cfb2fef48dc67b6c665f11ba840abf31d98d0b
Gaëtan H <steellgold0@gmail.com>
2021-06-08 20:53:31 +0200

    Add Staff Unicode Sex Chooser

--- Museum/src/Steellg0ld/Museum/forms/SettingsForm.php
+++ Museum/src/Steellg0ld/Museum/forms/SettingsForm.php
@@ -18,6 +18,8 @@ class SettingsForm{
                     if ($data !== null) {
                         $p->lang = Utils::LANGS[$data[2]];
                         $p->settings["economy_symbol"] = $data[1] + 1;
+                        $p->settings["unicode"] = $data[3] ?? 0;
+                        var_dump($data[3]);
                         Utils::sendMessage($p, "SETTINGS_UPDATED");
                     }
                 }
@@ -27,6 +29,9 @@ class SettingsForm{
             $form->addLabel(Utils::getMessage($player, "SETTINGS_LABEL_FORM", ["{RANK}"], [Ranks::translate($player,$player->getRank())]));
             $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_0"),[Economy::DOL,Economy::EUR,Economy::ARO,Economy::YEN]);
             $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_1"),[Utils::getMessage($player, "SETTINGS_DROPDOWN_1_0"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_1"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_2"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_3"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_4")],array_search($player->lang,Utils::LANGS));
+            if($player->hasRank(Ranks::HELPER,Ranks::MODERATOR,Ranks::ADMIN)){
+                $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_2"),[Utils::getMessage($player, "SETTINGS_DROPDOWN_2_0", ["[U]"], [Unicode::getMFace(0, $player->getRank())]), Utils::getMessage($player, "SETTINGS_DROPDOWN_2_1", ["[U]"], [Unicode::getMFace(1, $player->getRank())]),Utils::getMessage($player, "SETTINGS_DROPDOWN_2_2", ["[U]"], [Unicode::getMFace(2, $player->getRank())]),Utils::getMessage($player, "SETTINGS_DROPDOWN_2_3", ["[U]"], [Unicode::getMFace(3, $player->getRank())])],$player->settings['unicode']);
+            }
             $player->sendForm($form);
         }
     }
