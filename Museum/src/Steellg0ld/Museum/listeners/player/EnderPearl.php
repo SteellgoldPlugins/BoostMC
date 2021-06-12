51ce196469adc4747f6ad2a97dc0f532fc23ece0
Gaëtan H <steellgold0@gmail.com>
2021-06-12 23:25:49 +0200

    EnderPearl save next time on launch

---
+++ Museum/src/Steellg0ld/Museum/listeners/player/EnderPearl.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Steellg0ld\Museum\listeners\player;
+
+use pocketmine\event\entity\ProjectileLaunchEvent;
+use pocketmine\event\Listener;
+use Steellg0ld\Museum\base\Player;
+use pocketmine\entity\projectile\EnderPearl as EP;
+
+class EnderPearl implements Listener{
+    public function enderpearlLauncheEvent(ProjectileLaunchEvent $event){
+        $entity = $event->getEntity();
+        if ($entity instanceof EP){
+            $player = $entity->getOwningEntity();
+            if ($player instanceof Player and $player->isOnline()) {
+                $player->nextEnderPearl = time() + 5;
+            }
+        }
+    }
+}
