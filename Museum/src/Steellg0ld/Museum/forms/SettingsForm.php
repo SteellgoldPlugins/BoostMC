dedaf94457575187add2ed9f4f3452edefb987ae
Gaëtan H <steellgold0@gmail.com>
2021-06-13 00:43:43 +0200

    Add ArmorPvp Status in Settings Form

--- Museum/src/Steellg0ld/Museum/forms/SettingsForm.php
+++ Museum/src/Steellg0ld/Museum/forms/SettingsForm.php
@@ -19,7 +19,7 @@ class SettingsForm{
                         $p->lang = Utils::LANGS[$data[2]];
                         $p->settings["economy_symbol"] = $data[1] + 1;
                         $p->settings["unicode"] = $data[3] ?? 0;
-                        var_dump($data[3]);
+                        $p->settings["armor_pvp_status"] = $data[4] ?? false;
                         Utils::sendMessage($p, "SETTINGS_UPDATED");
                     }
                 }
@@ -29,6 +29,7 @@ class SettingsForm{
             $form->addLabel(Utils::getMessage($player, "SETTINGS_LABEL_FORM", ["{RANK}"], [Ranks::translate($player,$player->getRank())]));
             $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_0"),[Economy::DOL,Economy::EUR,Economy::ARO,Economy::YEN]);
             $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_1"),[Utils::getMessage($player, "SETTINGS_DROPDOWN_1_0"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_1"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_2"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_3"),Utils::getMessage($player, "SETTINGS_DROPDOWN_1_4")],array_search($player->lang,Utils::LANGS));
+            $form->addToggle(Utils::getMessage($player, "SETTINGS_TOGGLE_1"),$player->settings["armor_pvp_status"]);
             if($player->hasRank(Ranks::HELPER,Ranks::MODERATOR,Ranks::ADMIN)){
                 $form->addDropdown(Utils::getMessage($player, "SETTINGS_DROPDOWN_2"),[Utils::getMessage($player, "SETTINGS_DROPDOWN_2_0", ["[U]"], [Unicode::getMFace(0, $player->getRank())]), Utils::getMessage($player, "SETTINGS_DROPDOWN_2_1", ["[U]"], [Unicode::getMFace(1, $player->getRank())]),Utils::getMessage($player, "SETTINGS_DROPDOWN_2_2", ["[U]"], [Unicode::getMFace(2, $player->getRank())]),Utils::getMessage($player, "SETTINGS_DROPDOWN_2_3", ["[U]"], [Unicode::getMFace(3, $player->getRank())])],$player->settings['unicode']);
             }
