ff865d9f23635e5c7b0eae372d5ed7e9a3057769
Gaëtan H <steellgold0@gmail.com>
2021-06-13 00:43:34 +0200

    Uses

--- Museum/src/Steellg0ld/Museum/commands/defaults/Test.php
+++ Museum/src/Steellg0ld/Museum/commands/defaults/Test.php
@@ -4,8 +4,10 @@ namespace Steellg0ld\Museum\commands\defaults;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\entity\Entity;
 use pocketmine\Server;
 use Steellg0ld\Museum\base\Player;
+use Steellg0ld\Museum\entity\Wither;
 use Steellg0ld\Museum\forms\Emoticons;
 use Steellg0ld\Museum\forms\faction\MemberForm;
 use Steellg0ld\Museum\forms\FactionForm;
