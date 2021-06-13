4f19dd1183d0baef410bf270510ca31497d87991
Gaëtan H <steellgold0@gmail.com>
2021-06-13 15:48:34 +0200

    Add Netherite to Utils Unicodes

--- Museum/src/Steellg0ld/Museum/utils/Utils.php
+++ Museum/src/Steellg0ld/Museum/utils/Utils.php
@@ -9,6 +9,10 @@ use pocketmine\Server;
 use Steellg0ld\Museum\base\Economy;
 use Steellg0ld\Museum\base\Faction;
 use Steellg0ld\Museum\base\Player;
+use Steellg0ld\Museum\custom\armor\NetheriteBoots;
+use Steellg0ld\Museum\custom\armor\NetheriteChestplate;
+use Steellg0ld\Museum\custom\armor\NetheriteHelmet;
+use Steellg0ld\Museum\custom\armor\NetheriteLeggings;
 use Steellg0ld\Museum\Plugin;
 
 class Utils {
@@ -16,10 +20,10 @@ class Utils {
     CONST REPLACE = ["\n","\n","§l» §r§f", Unicode::EXCLAMATION." ", Unicode::RED_EXCLAMATION." ", Unicode::HELP." ", Unicode::RED_HELP." ", Unicode::PLUS." ", Unicode::LESS." ", "§a", "§f", "§7", "§f", "§c", "§4", Faction::DEFAULT_FACTION_PRICE];
     CONST LANGS = [0 => "fr_FR", 1 => "en_EN", 2 => "es_ES", 3 => "it_IT", 4 => "ch_CH"];
 
-    CONST HELMET = [ItemIds::LEATHER_HELMET => 55, ItemIds::CHAIN_HELMET => 165, ItemIds::IRON_HELMET => 165, ItemIds::GOLDEN_HELMET => 77, ItemIds::DIAMOND_HELMET => 363];
-    CONST CHESTPLATES = [ItemIds::LEATHER_CHESTPLATE => 80, ItemIds::CHAIN_CHESTPLATE => 240, ItemIds::IRON_CHESTPLATE => 240, ItemIds::GOLDEN_CHESTPLATE => 112, ItemIds::DIAMOND_CHESTPLATE => 528];
-    CONST LEGGINGS = [ItemIds::LEATHER_LEGGINGS => 75, ItemIds::CHAIN_LEGGINGS => 225, ItemIds::IRON_LEGGINGS => 225, ItemIds::GOLDEN_LEGGINGS => 105, ItemIds::DIAMOND_LEGGINGS => 495];
-    CONST BOOTS = [ItemIds::LEATHER_BOOTS => 65, ItemIds::CHAIN_BOOTS => 195, ItemIds::IRON_BOOTS => 195, ItemIds::GOLDEN_BOOTS => 91, ItemIds::DIAMOND_BOOTS => 429];
+    CONST HELMET = [ItemIds::LEATHER_HELMET => 55, ItemIds::CHAIN_HELMET => 165, ItemIds::IRON_HELMET => 165, ItemIds::GOLDEN_HELMET => 77, ItemIds::DIAMOND_HELMET => 363, NetheriteHelmet::NETHERITE_HELMET => 407];
+    CONST CHESTPLATES = [ItemIds::LEATHER_CHESTPLATE => 80, ItemIds::CHAIN_CHESTPLATE => 240, ItemIds::IRON_CHESTPLATE => 240, ItemIds::GOLDEN_CHESTPLATE => 112, ItemIds::DIAMOND_CHESTPLATE => 528, NetheriteChestplate::NETHERITE_CHESTPLATE => 592];
+    CONST LEGGINGS = [ItemIds::LEATHER_LEGGINGS => 75, ItemIds::CHAIN_LEGGINGS => 225, ItemIds::IRON_LEGGINGS => 225, ItemIds::GOLDEN_LEGGINGS => 105, ItemIds::DIAMOND_LEGGINGS => 495, NetheriteLeggings::NETHERITE_LEGGINGS => 555];
+    CONST BOOTS = [ItemIds::LEATHER_BOOTS => 65, ItemIds::CHAIN_BOOTS => 195, ItemIds::IRON_BOOTS => 195, ItemIds::GOLDEN_BOOTS => 91, ItemIds::DIAMOND_BOOTS => 429, NetheriteBoots::NETHERITE_BOOTS => 481];
 
     public static function getPrefix(): string{
         return "§l» §r§f";
