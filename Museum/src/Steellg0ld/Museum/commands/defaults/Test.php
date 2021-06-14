bdf27b86ff4ede383af0d1f15c75cb8355053fdb
Gaëtan H <steellgold0@gmail.com>
2021-06-14 21:11:59 +0200

    ManualClearLagg

--- Museum/src/Steellg0ld/Museum/commands/defaults/Test.php
+++ Museum/src/Steellg0ld/Museum/commands/defaults/Test.php
@@ -5,6 +5,8 @@ namespace Steellg0ld\Museum\commands\defaults;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\entity\Entity;
+use pocketmine\entity\object\ExperienceOrb;
+use pocketmine\entity\object\ItemEntity;
 use pocketmine\Server;
 use Steellg0ld\Museum\base\Player;
 use Steellg0ld\Museum\entity\Wither;
@@ -25,6 +27,25 @@ class Test extends Command {
         if($sender instanceof Player){
             if($args[0] == "1"){
                 Emoticons::manage($sender);
+            }elseif($args[0] == "2"){
+                $i = 0;
+                $e = 0;
+                foreach(Server::getInstance()->getLevels() as $level){
+                    foreach($level->getEntities() as $entity){
+                        if($entity instanceof ItemEntity){
+                            $entity->close();
+                            $i++;
+                        }
+
+                        if($entity instanceof ExperienceOrb){
+                            $entity->close();
+                            $e++;
+                        }
+                    }
+                }
+
+                $sender->sendMessage($i . " items ont étées supprimées");
+                $sender->sendMessage($e . " orbes d'experience ont étées supprimées");
             }else{
                 Entity::createEntity(Wither::WITHER,$sender->getLevel(),Entity::createBaseNBT($sender));
             }
