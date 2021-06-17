11e5e4677d91a01b7b75430121e1ac1c892b7b53
Gaëtan H <steellgold0@gmail.com>
2021-06-17 21:00:56 +0200

    MySQL creditentials

--- Museum/src/confidential/MySQL.php
+++ Museum/src/confidential/MySQL.php
@@ -3,8 +3,8 @@
 namespace confidential;
 
 class MySQL {
-    const HOSTNAME = "localhost";
-    const USER = "root";
+    const HOSTNAME = "127.0.0.1";
+    const USER = "boostmc";
     const PASSWORD = "ecsvhAw2RN#?d9&mVMK0QxQeVJVVQENIvlj4DEgc";
     const DATABASE = "faction";
 }
