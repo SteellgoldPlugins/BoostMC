9cb270567cee8a43ff2633afbc41fa83717f4aa2
Gaëtan H <steellgold0@gmail.com>
2021-06-21 21:17:36 +0200

    Economy edit symbols, add Unicode

--- Museum/src/Steellg0ld/Museum/base/Economy.php
+++ Museum/src/Steellg0ld/Museum/base/Economy.php
@@ -3,20 +3,23 @@
 namespace Steellg0ld\Museum\base;
 
 use Steellg0ld\Museum\utils\Prices;
+use Steellg0ld\Museum\utils\Unicode;
 
 class Economy
 {
     public const SYMBOLS = [
-        1 => self::DOL,
-        2 => self::EUR,
-        3 => self::ARO,
-        4 => self::YEN
+        1 => self::UNICODE,
+        2 => self::DOL,
+        3 => self::EUR,
+        4 => self::ARO,
+        5 => self::YEN
     ];
 
     const DOL = "$";
     const EUR = "€";
     const ARO = "@";
     const YEN = "¥";
+    const UNICODE = Unicode::COIN;
 
     public function addMoney(Player $player, Int $count){
         $player->money = $player->money + $count;
